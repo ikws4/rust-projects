a257c8963766c2fee1ea15c3db28111293cd9852
ikws4 <zhipingne@gmail.com>
2025-02-04 23:20:36 +0800

    feat: juice example

--- juice/src/main.rs
+++ juice/src/main.rs
@@ -1,9 +1,38 @@
+use juice::{flow::Flow, interpreter::Interpreter};
+use lexer::Lexer;
+use parser::Parser;
+
 pub mod ast;
 pub mod juice;
 pub mod lexer;
 pub mod parser;
 pub mod token;
 
+fn eval(source: &str) {
+    let tokens = Lexer::new(source).lex();
+    let statements = Parser::new(tokens).parse();
+    let result = Interpreter::new()
+        .with_std()
+        .unwrap()
+        .interprete(&statements);
+
+    if let Err(flow) = &result {
+        if let Flow::Error(msg) = flow {
+            panic!("{}", msg);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(filename) = std::env::args().nth(1) {
+        if !filename.ends_with(".juice") {
+            panic!("File must have .juice extension");
+        }
+        match std::fs::read_to_string(filename) {
+            Ok(source) => eval(&source),
+            Err(err) => panic!("Error reading file: {}", err),
+        }
+    } else {
+        println!("Please provide a filename as argument");
+    }
 }
